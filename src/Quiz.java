31eb8b5f90191bd7298d8f54cde43af00e42da39
Gundeep <Gundeep@DESKTOP-L10JNPV>
2019-05-28 16:12:58 -0400

    part 1

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,4 +1,11 @@
 
 public class Quiz {
+	public static void main(String args[]) {
+
+		String s1 = "copyandpastecopyandpastecopyandpastecopyandpastecopyandpastecopyandpastecommitandpushcommitandpushcommitandpushcommitandpushpushcccccommmitttsss";
+		//The total number of characters in the sentence
+		int num = s1.length();
+		System.out.println("Length of string s1 is : " + num);
+	}
 
 }
