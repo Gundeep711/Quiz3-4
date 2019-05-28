659fc56382388fa705cbe77ccea6ef1b4c747c06
Gundeep <Gundeep@DESKTOP-L10JNPV>
2019-05-28 16:37:20 -0400

    part 3

--- src/Quiz.java
+++ src/Quiz.java
@@ -6,6 +6,31 @@ public class Quiz {
 		//The total number of characters in the sentence
 		int num = s1.length();
 		System.out.println("Length of string s1 is : " + num);
+		
+		String sub=s1.substring(129);
+		System.out.println("The last 15 characters in the string is "+sub);
+
+		// part 3
+		int count=0;
+		int count1=0;
+		
+		for(int i=0;i<s1.length();i++)
+		{
+			if(s1.charAt(i)=='c')
+			{
+				count++;
+			}
+			else if(s1.charAt(i)=='p')
+			{
+				count1++;
+			}
+			
+		}
+		
+		System.out.println("Number of time c appears is "+count);
+		System.out.println("Number of time p appears is "+count1);
+		
+		
 	}
 
 }
